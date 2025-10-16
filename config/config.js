b65f830306958a060484b598ae771a0cba7e3df9
kevinluna94 <kevinemanuelluna@gmail.com>
2025-10-15 21:30:57 -0300

    cambios en config.js

--- config/config.js
+++ config/config.js
@@ -1,22 +1,16 @@
 require('dotenv').config();
+console.log('ENV DB_URL:', process.env.DB_URL);
+console.log('ENV DATABASE_URL:', process.env.DATABASE_URL);
 
 module.exports = {
   development: {
     url: process.env.DB_URL,
     dialect: 'mysql',
     logging: false,
-    define: {
-      underscored: true,
-    },
-  },
-  test: {
-    url: process.env.DB_URL,
-    dialect: 'mysql',
-    logging: false,
   },
   production: {
-    url: process.env.DB_URL,
+    url: process.env.DB_URL || process.env.DATABASE_URL,
     dialect: 'mysql',
-    logging: false,
+    logging: console.log, // muestra queries en los logs
   },
 };
