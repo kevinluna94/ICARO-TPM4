333be0f2efef4032f82f93926983e296fc3f1562
kevinluna94 <kevinemanuelluna@gmail.com>
2025-10-15 21:15:17 -0300

    agregue dependencias en package.json

--- migrations/20251007214224-create-products.js
+++ migrations/20251007214224-create-products.js
@@ -2,13 +2,20 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
- async up(queryInterface, Sequelize) {
-  const table = await queryInterface.describeTable('products');
-  if (!table.image_url) {
-    await queryInterface.addColumn('products', 'image_url', {
-      type: Sequelize.STRING,
-      allowNull: true,
-    });
+  async up(queryInterface, Sequelize) {
+    const table = await queryInterface.describeTable('products');
+    if (!table.image_url) {
+      await queryInterface.addColumn('products', 'image_url', {
+        type: Sequelize.STRING,
+        allowNull: true,
+      });
+    }
+  },
+
+  async down(queryInterface, Sequelize) {
+    const table = await queryInterface.describeTable('products');
+    if (table.image_url) {
+      await queryInterface.removeColumn('products', 'image_url');
+    }
   }
-}
-}
+};
